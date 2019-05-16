228c0099b0c0e7723c892036648bd4ebc3e95195
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-05-16 12:17:50 +0300

    Remove unused syslog functions

--- sys_util/src/syslog.rs
+++ sys_util/src/syslog.rs
@@ -29,19 +29,11 @@ use std::fs::File;
 use std::io;
 use std::io::{stderr, Cursor, ErrorKind, Write};
 use std::mem;
-use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
 use std::path::PathBuf;
 use std::ptr::null;
 use std::sync::{MutexGuard, Once, ONCE_INIT};
 
-use libc::{
-    closelog, fcntl, localtime_r, openlog, time, time_t, tm, F_GETFD, LOG_NDELAY, LOG_PERROR,
-    LOG_PID, LOG_USER,
-};
-
 use sync::Mutex;
-
-use crate::getpid;
 
 const SYSLOG_PATH: &str = "/dev/log";
 
@@ -274,29 +266,6 @@ pub fn echo_stderr(enable: bool) {
     state.stderr = enable;
 }
 
-/// Retrieves the file descriptors owned by the global syslogger.
-///
-/// Does nothing if syslog was never initialized. If their are any file descriptors, they will be
-/// pushed into `fds`.
-///
-/// Note that the `stderr` file descriptor is never added, as it is not owned by syslog.
-pub fn push_fds(fds: &mut Vec<RawFd>) {
-    let state = lock!();
-    fds.extend(state.file.iter().map(|f| f.as_raw_fd()));
-}
-
-fn get_localtime() -> tm {
-    unsafe {
-        // Safe because tm is just a struct of plain data.
-        let mut tm: tm = mem::zeroed();
-        let mut now: time_t = 0;
-        // Safe because we give time a valid pointer and can never fail.
-        time(&mut now as *mut _);
-        // Safe because we give localtime_r valid pointers and can never fail.
-        localtime_r(&now, &mut tm as *mut _);
-        tm
-    }
-}
 
 /// Records a log message with the given details.
 ///
