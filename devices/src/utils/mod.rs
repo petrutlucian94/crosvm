769b14e4e8b2a14da2f6348117beef14c9334b2a
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-05-17 16:55:03 +0300

    Remove unused device utils modules
    Those helpers were relying on epoll, which isn't available on
    Windows.
    
    Luckily, those were only used for usb devices, which we dropped.

--- devices/src/utils/mod.rs
+++ devices/src/utils/mod.rs
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-mod async_job_queue;
 mod error;
-mod event_loop;
 
-pub use self::async_job_queue::*;
 pub use self::error::*;
-pub use self::event_loop::*;
